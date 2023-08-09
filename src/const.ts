63a3c7319bf7094ec40cf6839a5ea7471674d9ed
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-08-09 17:13:28 +0300

    wip: добавляет константы API маршрутов

---
+++ src/const.ts
@@ -0,0 +1,13 @@
+export const BASE_URL = 'https://cloud.iexapis.com';
+
+export enum APIRoute {
+  Stable = '/stable',
+  Stock = '/stock',
+  Market = '/market',
+  Collection = '/collection',
+  List = '/list'
+}
+
+
+// TODO: перенести в переменные окружения
+export const token = 'pk_da03ec1ae5264b05bb2e9172e1ce3cba';
