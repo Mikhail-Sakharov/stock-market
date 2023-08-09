0ea5f5e19b790c381ead5fcde0b422a1f47d12a0
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-08-09 21:40:57 +0300

    feat: добавляет пагинацию

--- src/components/stock-list/stock-list.tsx
+++ src/components/stock-list/stock-list.tsx
@@ -6,17 +6,24 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {useAppDispatch, useAppSelector} from '../../hooks';
-import {useEffect} from 'react';
+import {ChangeEvent, useEffect, useState} from 'react';
 import {fetchStocksAction} from '../../store/api-actions';
 import {CollectionName} from '../../types/collection-name.enum';
 import {token as userToken} from '../../const';
 import {getStocks} from '../../store/app-data/selectors';
 import {nanoid} from 'nanoid';
+import {Pagination} from '@mui/material';
+
+const MAX_STOCKS_PER_PAGE = 10;
 
 function StockList(): JSX.Element {
   const dispatch = useAppDispatch();
 
   const stocks = useAppSelector(getStocks);
+
+  const pagesCount = Math.ceil(stocks.length / MAX_STOCKS_PER_PAGE);
+
+  const [pageNumber, setPageNumber] = useState(1);
 
   const token = userToken;
 
@@ -27,49 +34,61 @@ function StockList(): JSX.Element {
     }));
   }, [dispatch, token]);
 
+  const handlePageNumberChange = (_event: ChangeEvent<unknown>, page: number) => {
+    setPageNumber(page);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Company Name</TableCell>
-            <TableCell align="right">Symbol</TableCell>
-            <TableCell align="right">Primary exchange</TableCell>
-            <TableCell align="right">MarketCap</TableCell>
-            <TableCell align="right">Change</TableCell>
-            <TableCell align="right">Ask</TableCell>
-            <TableCell align="right">Bid</TableCell>
-            <TableCell align="right">Latest</TableCell>
-            <TableCell align="right">IEX Volume</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {stocks.map((stock) => (
-            <TableRow
-              key={nanoid()}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {`${stock.companyName ?? '-'}`}
-              </TableCell>
-              <TableCell align="right">{`${stock.symbol ?? '-'}`}</TableCell>
-              <TableCell align="right">{`${stock.primaryExchange ?? '-'}`}</TableCell>
-              <TableCell align="right">{`${stock.marketCap ? stock.marketCap.toLocaleString() : '-'} ${stock.currency ?? ''}`}</TableCell>
-              <TableCell
-                align="right"
-                sx={{color: stock.change > 0 ? 'green' : 'red'}}
-              >
-                {`${stock.change ?? '-'} / ${stock.changePercent ?? '-'}`}
-              </TableCell>
-              <TableCell align="right">{stock.iexAskPrice ?? '-'}</TableCell>
-              <TableCell align="right">{stock.iexBidPrice ?? '-'}</TableCell>
-              <TableCell align="right">{stock.latestPrice ?? '-'}</TableCell>
-              <TableCell align="right">{stock.iexVolume ?? '-'}</TableCell>
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Company Name</TableCell>
+              <TableCell align="right">Symbol</TableCell>
+              <TableCell align="right">Primary exchange</TableCell>
+              <TableCell align="right">MarketCap</TableCell>
+              <TableCell align="right">Change</TableCell>
+              <TableCell align="right">Ask</TableCell>
+              <TableCell align="right">Bid</TableCell>
+              <TableCell align="right">Latest</TableCell>
+              <TableCell align="right">IEX Volume</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {stocks.slice(
+              (pageNumber - 1) * MAX_STOCKS_PER_PAGE,
+              MAX_STOCKS_PER_PAGE * pageNumber
+            ).map((stock) => (
+              <TableRow
+                key={nanoid()}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {`${stock.companyName ?? '-'}`}
+                </TableCell>
+                <TableCell align="right">{`${stock.symbol ?? '-'}`}</TableCell>
+                <TableCell align="right">{`${stock.primaryExchange ?? '-'}`}</TableCell>
+                <TableCell align="right">{`${stock.marketCap ? stock.marketCap.toLocaleString() : '-'} ${stock.currency ?? ''}`}</TableCell>
+                <TableCell
+                  align="right"
+                  sx={{color: stock.change > 0 ? 'green' : 'red'}}
+                >
+                  {`${stock.change ?? '-'} / ${stock.changePercent ?? '-'}`}
+                </TableCell>
+                <TableCell align="right">{stock.iexAskPrice ?? '-'}</TableCell>
+                <TableCell align="right">{stock.iexBidPrice ?? '-'}</TableCell>
+                <TableCell align="right">{stock.latestPrice ?? '-'}</TableCell>
+                <TableCell align="right">{stock.iexVolume ?? '-'}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div style={{alignSelf: 'center', marginTop: '10px'}}>
+        <Pagination count={pagesCount} onChange={handlePageNumberChange} variant="outlined"/>
+      </div>
+    </div>
   );
 }
 
