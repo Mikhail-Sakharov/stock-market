15d6947f7252b586114f87d55b622ae2e55bdc80
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-08-09 17:56:26 +0300

    wip: добавляет перечисление с именами слайсов редьюсера

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,7 @@
 export const BASE_URL = 'https://cloud.iexapis.com';
 
+export const REQUEST_TIMEOUT = 5000;
+
 export enum APIRoute {
   Stable = '/stable',
   Stock = '/stock',
@@ -12,3 +14,7 @@ export enum APIRoute {
 
 // TODO: перенести в переменные окружения
 export const token = 'pk_da03ec1ae5264b05bb2e9172e1ce3cba';
+
+export enum ReducerNameSpace {
+  AppData = 'APP_DATA'
+}
